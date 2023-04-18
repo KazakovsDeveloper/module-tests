7ed16556d42de8c05ce3c6401a77283f56512566
Maxim Kazakov <m.c.kazakov.developer@gmail.com>
2023-04-18 14:46:15 +0300

    add realisation for  solveTestCoefficientCanNotEqualsNanOrInfinity()

--- src/main/java/ru/otus/module/tests/SquareEquation.java
+++ src/main/java/ru/otus/module/tests/SquareEquation.java
@@ -1,6 +1,8 @@
 package ru.otus.module.tests;
 import org.apache.commons.math3.util.Precision;
 
+import static java.lang.Double.NaN;
+
 /**
  * Необходимо реализовать операцию нахождения квадратного уравнения. Предположим, что эта операция описывается следующей функцией с поправкой на конкретный язык программирования.
  * В ООП языках эта функция реализуется в виде метода класса.
@@ -15,6 +17,18 @@ public class SquareEquation {
         double epsilon = 0.000001d;
         if (Precision.equals(a, 0.0, epsilon)) {
             throw new RuntimeException("Коэффициент a не может быть = 0");
+        }
+
+        if (Double.isNaN(a) || Double.isInfinite(a)) {
+            throw new RuntimeException("Коэффициент а имеет некорректное значение a = " + a);
+        }
+
+        if (Double.isNaN(b) || Double.isInfinite(b)) {
+            throw new RuntimeException("Коэффициент b имеет некорректное значение b = " + b);
+        }
+
+        if (Double.isNaN(c) || Double.isInfinite(c)) {
+            throw new RuntimeException("Коэффициент c имеет некорректное значение c = " + c);
         }
 
         double discr = b * b - 4 * a * c;
