3a44628dca139738fd77dde5c4ffe234bc795106
Maxim Kazakov <m.c.kazakov.developer@gmail.com>
2023-04-18 14:31:26 +0300

    add realisation for solveTestCoefficientACanNotEquals0()

--- src/main/java/ru/otus/module/tests/SquareEquation.java
+++ src/main/java/ru/otus/module/tests/SquareEquation.java
@@ -1,4 +1,5 @@
 package ru.otus.module.tests;
+import org.apache.commons.math3.util.Precision;
 
 /**
  * Необходимо реализовать операцию нахождения квадратного уравнения. Предположим, что эта операция описывается следующей функцией с поправкой на конкретный язык программирования.
@@ -8,11 +9,17 @@ package ru.otus.module.tests;
  */
 
 public class SquareEquation {
-
     //ax2 + bx + c = 0
     public double[] solve(double a, double b, double c) {
+
+        double epsilon = 0.000001d;
+        if (Precision.equals(a, 0.0, epsilon)) {
+            throw new RuntimeException("Коэффициент a не может быть = 0");
+        }
+
         double discr = b * b - 4 * a * c;
         double[] result = new double[2];
+
         if (discr < 0) {
             return new double[0];
         } else if (discr > 0) {
