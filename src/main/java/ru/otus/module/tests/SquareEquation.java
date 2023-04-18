164918bbc1b92103ad5e011fafe2264411d52eda
Maxim Kazakov <m.c.kazakov.developer@gmail.com>
2023-04-18 15:25:58 +0300

    refactoring

--- src/main/java/ru/otus/module/tests/SquareEquation.java
+++ src/main/java/ru/otus/module/tests/SquareEquation.java
@@ -1,7 +1,8 @@
 package ru.otus.module.tests;
+
 import org.apache.commons.math3.util.Precision;
 
-import static java.lang.Double.NaN;
+import java.util.function.Predicate;
 
 /**
  * Необходимо реализовать операцию нахождения квадратного уравнения. Предположим, что эта операция описывается следующей функцией с поправкой на конкретный язык программирования.
@@ -15,19 +16,21 @@ public class SquareEquation {
     public double[] solve(double a, double b, double c) {
 
         double epsilon = 0.000001d;
-        if (Precision.equals(a, 0.0, epsilon)) {
+        boolean isEqualTo0 = Precision.equals(a, 0.0, epsilon);
+
+        Predicate<Double> isInfiniteOrNan = (doubleVal) -> Double.isInfinite(doubleVal) || Double.isNaN(doubleVal);
+
+        if (isEqualTo0) {
             throw new RuntimeException("Коэффициент a не может быть = 0");
         }
 
-        if (Double.isNaN(a) || Double.isInfinite(a)) {
+        if (isInfiniteOrNan.test(a)) {
             throw new RuntimeException("Коэффициент а имеет некорректное значение a = " + a);
         }
-
-        if (Double.isNaN(b) || Double.isInfinite(b)) {
+        if (isInfiniteOrNan.test(b)) {
             throw new RuntimeException("Коэффициент b имеет некорректное значение b = " + b);
         }
-
-        if (Double.isNaN(c) || Double.isInfinite(c)) {
+        if (isInfiniteOrNan.test(c)) {
             throw new RuntimeException("Коэффициент c имеет некорректное значение c = " + c);
         }
 
